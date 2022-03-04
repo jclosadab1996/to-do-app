487753f5c2c842bf6bfe860ff9f77c9192ffd4fc
Juan Camilo Losada <jclosadab1996@javerianacali.edu.co>
2022-03-04 01:22:48 -0500

    Crando las etiquetas ToDo en Js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { CreateTodoButton } from './CreateTodoButton';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+// import './App.css';
 
-function App(props) {
+const todos = [
+  {text: 'Cortar cebolla', completed: false },
+  {text: 'Tomar el curso de React JS', completed: false },
+  {text: 'Llorar', completed: false },
+]
+
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {props.children}
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter />
+
+      <TodoSearch />
+
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <App saludo="oli">
-    <h1>
-      Esto es título
-    </h1>
-  </App>,
+  <App />,
   document.getElementById('root')
 );
 
