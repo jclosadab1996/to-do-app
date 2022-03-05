cce27e5faf7bd4b16a3d23a32f8900ad1fcc33ef
Juan Camilo Losada <jclosadab1996@javerianacali.edu.co>
2022-03-04 20:18:19 -0500

    Estilando proyecto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { TodoItem } from './TodoItem';
 // import './App.css';
 
 const todos = [
-  {text: 'Cortar cebolla', completed: false },
+  {text: 'Cortar cebolla', completed: true },
   {text: 'Tomar el curso de React JS', completed: false },
   {text: 'Llorar', completed: false },
 ]
@@ -16,12 +16,15 @@ function App() {
   return (
     <React.Fragment>
       <TodoCounter />
-
       <TodoSearch />
 
       <TodoList>
         {todos.map(todo => (
-          <TodoItem key={todo.text} text={todo.text} />
+          <TodoItem 
+            key={todo.text} 
+            text={todo.text}
+            completed = {todo.completed} 
+          />
         ))}
       </TodoList>
 
